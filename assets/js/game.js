6a333aa129a1d0c76d0d72c29c83e69d25faaea5
allyssaangell <a.angell2@yahoo.com>
2022-01-10 19:01:46 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,15 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+
 var enemyAttack = 12;
+
+var randomNumber = function (min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+};
+var enemyHealth = randomNumber(40, 60);
 
 var fight = function (enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -22,14 +29,16 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + " has decided to skip this fight. Goodbye!");
         //subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
     //remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName +
         " attacked " +
@@ -53,7 +62,9 @@ var fight = function (enemyName) {
     }
 
     //remove player's health by subtracting the amount set in the enemyAttack variable.
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName +
         " attacked " +
@@ -89,7 +100,7 @@ var startGame = function () {
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
       //if player is still alive and we're not at the last enemy in the array
       if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -137,7 +148,6 @@ if (playAgainConfirm) {
 } else {
   window.alert("Thank you for playing Robot Gladiators! Come back soon!");
 }
-
 };
 
 var shop = function () {
